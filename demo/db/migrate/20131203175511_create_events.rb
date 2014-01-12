d72ea40022ee6fa52720e67f8493ab23ff4f140a
saasbook <saasbook@saasbook.(none)>
2014-01-12 08:42:16 -0800

    why

--- demo/db/migrate/20131203175511_create_events.rb
+++ demo/db/migrate/20131203175511_create_events.rb
@@ -10,6 +10,7 @@ class CreateEvents < ActiveRecord::Migration
       t.references :consumer
       t.integer :event_id
       t.integer :buy_id
+      t.integer :favorite_id
       t.timestamps
     end
   end

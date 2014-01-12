44de69c89cf615d4ad53bfa84a738925329af364
saasbook <saasbook@saasbook.(none)>
2014-01-12 08:04:01 -0800

    our baby please back

--- demo/app/controllers/usermanagement_controller.rb
+++ demo/app/controllers/usermanagement_controller.rb
@@ -2,5 +2,7 @@ class UsermanagementController < ApplicationController
   
    def userprofile
    @event = Event.includes(:consumer).where( :consumer_id => current_consumer.id)
+
+   @buy = Event.includes(:consumer).where( :buy_id => current_consumer.id)
    end
 end
